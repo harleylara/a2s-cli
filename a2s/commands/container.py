5095c10e74350a8fec05dc37b1454ae514fb7c03
harleylara <harley.lara@outlook.com>
2025-03-25 23:16:31 +0100

    add working prototype for starting a existing container

--- a2s/commands/container.py
+++ a2s/commands/container.py
@@ -1,14 +1,14 @@
 import subprocess
+from pathlib import Path
 from argparse import ArgumentParser, Namespace
 from a2s.commands import Command
 
 class Container(Command):
 
     def add_arguments(self, parser: ArgumentParser):
-        parser.add_argument("robot", type=str.lower, choices=["nao", "nala"])
+        parser.add_argument("robot", type=str.lower, choices=["base"])
 
     def execute(self, args: Namespace):
 
-        if args.robot == "nao":
-            print("nao setup")
-            subprocess.run(["apptainer", "help"], check=True)
+        if args.robot == "base":
+            subprocess.run(["apptainer", "shell", "base.sif"], check=True, cwd=Path().home() / ".a2s")
