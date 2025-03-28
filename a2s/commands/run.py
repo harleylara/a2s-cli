f4675848bae46d1c239b0a53941506aa6e6f91b6
harleylara <harley.lara@outlook.com>
2025-03-28 15:22:28 +0100

    add single cmd construction

--- a2s/commands/run.py
+++ a2s/commands/run.py
@@ -1,8 +1,8 @@
 import argparse
 import subprocess
-from pathlib import Path
 from argparse import ArgumentParser, Namespace
 from a2s.commands import Command
+from a2s.config import CONTAINERS_DIR, SUPPORTED_ROBOTS
 
 class Run(Command):
 
@@ -10,23 +10,27 @@ class Run(Command):
         """
 
         Arguments:
-        - `--raw`: if this parameter is passed, all other parameters are ignored
-            and everything after it is passed directly
-            to the internal command. Equivalently to `a2s run --raw <opts...>` -> `apptainer run <opts...>`.
-            Important: the underlying command is executed in the `~/.a2s` folder by default.
         """
-        parser.add_argument("robot", type=str.lower, choices=["base"])
-        parser.add_argument("--path", type=str.lower, default=Path().home() / ".a2s", choices=["base"], help="Path from where to run this command.")
-        parser.add_argument("--raw", nargs=argparse.REMAINDER, help="Pass raw arguments to underlying apptainer")
+        parser.add_argument("robot", type=str.lower, choices=SUPPORTED_ROBOTS)
+        parser.add_argument("--path", type=str.lower, default=CONTAINERS_DIR, help="Path from where to run this command.")
+        parser.add_argument(
+            "--raw",
+            nargs=argparse.REMAINDER, 
+            help="""
+if this parameter is passed, all other parameters are ignored
+and everything after it is passed directly
+to the internal command. Equivalently to `a2s run --raw <opts...>` -> `apptainer run <opts...>`.
+Important: the underlying command is executed in the `~/.a2s` folder by default."""
+        )
 
     def execute(self, args: Namespace):
 
         if args.raw:
             full_cmd = ["apptainer", "run"] + args.raw
-            subprocess.run(full_cmd, check=True, cwd=Path().home() / ".a2s")
+            subprocess.run(full_cmd, check=True, cwd=args.path)
             return
 
+        # TODO: FIX change shell for RUN but add a startscript
+        run_cmd = ["apptainer", "shell", f"{args.robot}.sif"]
+        subprocess.run(run_cmd, check=True, cwd=args.path)
 
-        # if args.robot == "base":
-        #     # subprocess.run(["apptainer", "run", "--bind", "/run/user:/run/user", "base.sif"], check=True, cwd=Path().home() / ".a2s")
-        #     subprocess.run(["apptainer", "run", "base.sif"], check=True, cwd=Path().home() / ".a2s")
